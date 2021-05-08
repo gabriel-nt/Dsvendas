4abd913b16863fc484d75e33aee74c5c610bd30d
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-05-08 11:08:37 -0300

    Pagination

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,30 @@
+import { SalePage } from "types/sale";
+
+type Props = {
+  page: SalePage
+  onPageChange: (index: number) => void;
+}
+
+const Pagination = ({ page, onPageChange }: Props) => (
+  <div className="row d-flex justify-content-center">
+    <nav>
+      <ul className="pagination">
+        <li className={`page-item ${page.first && 'disabled'}`}>
+          <button className="page-link" onClick={() => {
+            onPageChange(page.number - 1)
+          }}>Anterior</button>
+        </li>
+        <li className="page-item disabled">
+          <span className="page-link">{page.number + 1}</span>
+        </li>
+        <li className={`page-item ${page.last && 'disabled'}`}>
+          <button className="page-link" onClick={() => {
+            onPageChange(page.number + 1)
+          }}>Próxima</button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Pagination;
--- frontend/src/components/Table/index.tsx
+++ frontend/src/components/Table/index.tsx
@@ -1,49 +1,58 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const Table = () => {
+  const [activePage, setActivePage] = useState(0);
   const [page, setPage] = useState<SalePage>({} as SalePage);
 
   useEffect(() => {
     async function loadData() {
-      const response = await axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`);
+      const response = await axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`);
       setPage(response.data)
     }
 
     loadData();
-  }, []);
+  }, [activePage]);
+
+  const changePage = (index: number) => {
+    setActivePage(index);
+  }
 
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            page.content && page.content.map(row => (
-              <tr key={row.id}>
-                <td>{formatLocalDate(row.date, 'dd/MM/yyyy')}</td>
-                <td>{row.seller.name}</td>
-                <td>{row.visited}</td>
-                <td>{row.deals}</td>
-                <td>{row.amount}</td>
-              </tr>
-            ))
-          }
-          
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              page.content && page.content.map(row => (
+                <tr key={row.id}>
+                  <td>{formatLocalDate(row.date, 'dd/MM/yyyy')}</td>
+                  <td>{row.seller.name}</td>
+                  <td>{row.visited}</td>
+                  <td>{row.deals}</td>
+                  <td>{row.amount}</td>
+                </tr>
+              ))
+            }
+
+          </tbody>
+        </table>
+      </div>
+      <Pagination page={page} onPageChange={changePage}/>
+    </>
   )
 }
 
