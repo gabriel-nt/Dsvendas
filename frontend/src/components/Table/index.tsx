097597b4459ff14b85cecedd957f976677f60def
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-05-08 10:51:51 -0300

    DataTable integration

--- frontend/src/components/Table/index.tsx
+++ frontend/src/components/Table/index.tsx
@@ -1,61 +1,50 @@
-const Table = () => (
-  <div className="table-responsive">
-    <table className="table table-striped table-sm">
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Vendedor</th>
-          <th>Clientes visitados</th>
-          <th>Negócios fechados</th>
-          <th>Valor</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>22/04/2021</td>
-          <td>Barry Allen</td>
-          <td>34</td>
-          <td>25</td>
-          <td>15017.00</td>
-        </tr>
-        <tr>
-          <td>22/04/2021</td>
-          <td>Barry Allen</td>
-          <td>34</td>
-          <td>25</td>
-          <td>15017.00</td>
-        </tr>
-        <tr>
-          <td>22/04/2021</td>
-          <td>Barry Allen</td>
-          <td>34</td>
-          <td>25</td>
-          <td>15017.00</td>
-        </tr>
-        <tr>
-          <td>22/04/2021</td>
-          <td>Barry Allen</td>
-          <td>34</td>
-          <td>25</td>
-          <td>15017.00</td>
-        </tr>
-        <tr>
-          <td>22/04/2021</td>
-          <td>Barry Allen</td>
-          <td>34</td>
-          <td>25</td>
-          <td>15017.00</td>
-        </tr>
-        <tr>
-          <td>22/04/2021</td>
-          <td>Barry Allen</td>
-          <td>34</td>
-          <td>25</td>
-          <td>15017.00</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-)
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+const Table = () => {
+  const [page, setPage] = useState<SalePage>({} as SalePage);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`);
+      setPage(response.data)
+    }
+
+    loadData();
+  }, []);
+
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Vendedor</th>
+            <th>Clientes visitados</th>
+            <th>Negócios fechados</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            page.content && page.content.map(row => (
+              <tr key={row.id}>
+                <td>{formatLocalDate(row.date, 'dd/MM/yyyy')}</td>
+                <td>{row.seller.name}</td>
+                <td>{row.visited}</td>
+                <td>{row.deals}</td>
+                <td>{row.amount}</td>
+              </tr>
+            ))
+          }
+          
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
 export default Table;
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,14 @@
+import { Seller } from './seller'
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller
+}
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
@@ -7,4 +18,16 @@ export type SaleSuccess = {
   sellerName: string;
   deals: number;
   visited: number;
+}
+
+export type SalePage = {
+  content: Sale[];
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
 }
