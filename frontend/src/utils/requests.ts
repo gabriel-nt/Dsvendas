31bca649b0c4915bc2adc83e72bae29667f3f8dd
Gabriel Teixeira <49319947+Gabriel-Teixeira@users.noreply.github.com>
2021-05-08 21:42:56 -0300

    Update url

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
 console.log(process);
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://app-dsvendas-gabriel.herokuapp.com/';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://app-dsvendas-gabriel.herokuapp.com';
